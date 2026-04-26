5bf14675a08a2ab05e81437bef7d74f28810fedb
Emma Pang <emmapang3@gmail.com>
2026-04-26 15:51:12 +0200

    minor styling improvements for the label

--- src/utils/dynamicBackgroundColor.ts
+++ src/utils/dynamicBackgroundColor.ts
@@ -1,16 +1,21 @@
 export const dynamicBackgroundColors = () => {
   const currentTime = new Date().getHours();
   const heading = document.getElementById("main-heading") as HTMLHeadingElement;
+  const label = document.getElementById("cityInputLabel") as HTMLLabelElement;
 
   if (currentTime >= 4 && currentTime < 10) {
     document.body.classList.add("from-amber-200", "to-cyan-200");
+    label.classList.add("text-black");
   } else if (currentTime >= 10 && currentTime < 16) {
     document.body.classList.add("from-cyan-200", "to-blue-400");
+    label.classList.add("text-black");
   } else if (currentTime >= 16 && currentTime < 22) {
     document.body.classList.add("from-purple-400", "to-indigo-800");
     heading.classList.add("text-white");
+    label.classList.add("text-white");
   } else {
     document.body.classList.add("from-blue-950", "to-slate-950");
     heading.classList.add("text-white");
+    label.classList.add("text-white");
   }
 };
